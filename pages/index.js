6201985c1624713cc4d6475434bb3faf136cf1f7
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-03-30 14:48:56 +0900

    ♻️ Main部分のコンポーネント化

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,17 @@
-import Head from 'next/head'
-import { Footer } from '../components/Footer'
-import  { Headline } from '../components/Headline'
-import { Links } from '../components/Links'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { Footer } from '../components/Footer';
+import { Main } from '../components/Main';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <main className={styles.main}>
-        <Headline page="index" />
-
-        <Links />
-      </main>
-
+      <Main page='index'/>
       <Footer />
     </div>
-  )
+  );
 }
