2680e78d3af728bca59441375ee98062a236141b
thurendous <thurendous2@gmail.com>
2022-11-30 12:57:56 +0900

    ⚗️ for testing click event

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,27 @@ import { Footer } from 'src/components/Footer';
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main';
 import styles from 'src/styles/Home.module.css';
+import { useCallback } from 'react';
+
+
+
+console.log(11123123123)
 
 export default function Home() {
+
+  const handelClick = useCallback((e) => {
+    console.log(e.target.href)
+    e.preventDefault;
+    console.log(1233)
+    alert(321)}, [])
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
+      {/* <button onClick={() => alert(123)}>Button</button> */}
+      <a href="/about" onClick={handelClick}>Button</a>
       <Main page='index' />
       <Footer />
     </div>
